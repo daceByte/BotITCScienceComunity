a922077ec52992e67871f7621640d035ad8ad834
DACE <78763338+daceByte@users.noreply.github.com>
2023-09-10 20:38:12 -0500

    Fix start bot

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-const wppconnect = require('@wppconnect-team/wppconnect');
-const winston = require('winston');
-const command = require('./commands/index');
+const wppconnect = require("@wppconnect-team/wppconnect");
+const winston = require("winston");
+const command = require("./commands/index");
 
 const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    defaultMeta: {
-        service: 'user-service'
-    },
-    transports: [
-        //
-        // - Write all logs with level `error` and below to `error.log`
-        // - Write all logs with level `info` and below to `combined.log`
-        //
-        new winston.transports.File({
-            filename: 'error.log',
-            level: 'error'
-        }),
-        new winston.transports.File({
-            filename: 'combined.log'
-        }),
-    ],
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: {
+    service: "user-service",
+  },
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new winston.transports.File({
+      filename: "error.log",
+      level: "error",
+    }),
+    new winston.transports.File({
+      filename: "combined.log",
+    }),
+  ],
 });
 
 wppconnect
-    .create({
-        session: 'itcscience',
-        //logger: logger,
-        headless: true,
-        logQR: false,
-        browserArgs: [
-            '--no-sandbox',
-        ],
-        //puppeteerOptions:{
-            //executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe', // Ruta al ejecutable de Microsoft Edge en tu sistema
-        //}
-    })
-    .then(async (client) => {
-        await command.start(client);
-    })
-    .catch((erro) => {
-        console.log(erro);
-    });
+  .create({
+    session: "itcscience",
+    //logger: logger,
+    headless: true,
+    logQR: false,
+    disableWelcome: true,
+    useChrome: false,
+    browserArgs: ["--no-sandbox"],
+    //puppeteerOptions:{
+    //executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe', // Ruta al ejecutable de Microsoft Edge en tu sistema
+    //}
+  })
+  .then(async (client) => {
+    await command.start(client);
+  })
+  .catch((erro) => {
+    console.log(erro);
+  });
